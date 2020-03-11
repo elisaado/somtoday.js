c325d5dc70f7dafb5265e842f8d0dc49e228be25
Larshoorweg <62012409+Larshoorweg@users.noreply.github.com>
2020-03-11 11:20:52 +0100

    Leering update

--- leerling.ts
+++ leerling.ts
@@ -1,3 +1,12 @@
 class Leerling {
-
+      Type: string;
+      Leerlingnummer: int;
+      Roepnaam: string;
+      Achternaam: string;
+      Email: string;
+      MobielNummer: string;
+      Geboortedatum: Date;
+      Geslacht: string;
+      
 }
+   

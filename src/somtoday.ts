9b8f63c654c04cf0696f6661c77e96127f2f8174
Eli Saado <eli5saado@gmail.com>
2020-03-22 16:49:03 +0100

    Create main library class and add some fields

---
+++ src/somtoday.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { Leerling } from './leerling';
+
+class SOMToday {
+  _serversBaseURL: string = 'https://servers.somtoday.nl';
+  _authBaseURL: string = 'https://production.somtoday.nl';
+  _baseURL: string;
+  _accessToken: string;
+  _refreshToken: string;
+  _idToken: string;
+  
+}

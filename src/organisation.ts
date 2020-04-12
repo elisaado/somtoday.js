f3c7eeece3f8462ef25458fc6dc063ebb7df23be
Eli Saado <eli5saado@gmail.com>
2020-04-12 23:21:26 +0200

    export organisation as default

--- src/organisation.ts
+++ src/organisation.ts
@@ -1,7 +1,9 @@
-export class Organisation {
+class Organisation {
   constructor(
     public uuid: string,
     public name: string,
     public location: string,
   ) {}
 }
+
+export { Organisation as default };
--- src/somtoday.ts
+++ src/somtoday.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Organisation } from './organisation';
+import Organisation from './organisation';
 
 import Fuse = require('fuse.js');
 

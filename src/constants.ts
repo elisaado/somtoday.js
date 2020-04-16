24b5529c38720b2f3633b0dc7c1f250aa65cfec3
Eli Saado <eli5saado@gmail.com>
2020-04-16 13:26:14 +0200

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+const APP_ID = 'D50E0C06-32D1-4B41-A137-A9A850C892C2';
+const APP_SECRET = 'vDdWdKwPNaPCyhCDhaCnNeydyLxSGNJX';
+
+const APP_KEY = Buffer.from(`${APP_ID}:${APP_SECRET}`).toString('base64');
+
+export { APP_ID, APP_SECRET, APP_KEY };

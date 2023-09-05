f6e8aa8ebd926d9341378badf5b5d307d85c847f
FurriousFox <klokhuiswertas@gmail.com>
2023-09-05 20:37:11 +0200

    Fix authentication

--- src/user.ts
+++ src/user.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import Debug from "debug";
 import axios, { AxiosBasicCredentials, AxiosRequestConfig } from "axios";
 import { APP_ID } from "./constants";
@@ -47,33 +48,29 @@ class User extends baseApiClass {
         }
       });
     } else {
-      const requestInfo: AxiosRequestConfig = {
-        method: "POST",
-        baseURL: `https://somtoday.nl/oauth2/token`,
-        data: qs.stringify({
-          grant_type: "password",
-          username: `${credentials.organization}\\${credentials.username}`,
-          password: credentials.password,
-          scope: "openid",
-          client_id: APP_ID,
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }),
-      };
-      log("Fetching auth info");
-      axios
-        .request(requestInfo)
-        .then((res) => res.data)
-        .then((data: api_auth_item) => {
-          this._storeInfo(data);
-          this._authenticatedResolver(this);
-          this.axiosOptions = {
-            method: "get",
-            baseURL: `${this.somtodayApiUrl}` + endpoints.baseURL,
-            headers: { Authorization: `Bearer ${this.accessToken}` },
-          };
-        });
+      (async () => {
+        const https = require('https');
+        let nothing = () => { };
+        let [a, cookie] = await new Promise((resolve, reject) => https.get(`https://inloggen.somtoday.nl/oauth2/authorize?redirect_uri=somtodayleerling%3A%2F%2Foauth%2Fcallback&client_id=D50E0C06-32D1-4B41-A137-A9A850C892C2&response_type=code&prompt=login&scope=openid&tenant_uuid=${credentials.organization}&session=no_session`, (r) => resolve(nothing(r.on('data', () => { })) ?? [r.headers.location.split('?auth=')[1], r.headers['set-cookie'][0].split(';')[0]])));
+        let cookie2 = await new Promise((resolve, reject) => https.get(`https://inloggen.somtoday.nl/?auth=${a}`, { headers: { "Cookie": cookie } }, (r) => resolve(nothing(r.on('data', () => { })) ?? r.headers['set-cookie']?.[0]?.split(';')?.[0] ?? false)));
+
+        let c, d, r;
+        if (cookie2 !== false) {
+          let b1 = await new Promise((resolve, reject) => (r = https.request(`https://inloggen.somtoday.nl/?0-1.-panel-signInForm&auth=${a}`, { method: "POST", headers: { 'Origin': 'https://inloggen.somtoday.nl', 'Content-Type': 'application/x-www-form-urlencoded', 'Cookie': cookie + "; " + cookie2 } }, (r) => r.on('data', (b) => { }).on('end', () => resolve())), r.end(`loginLink=x&usernameFieldPanel%3AusernameFieldPanel_body%3AusernameField=${encodeURIComponent(credentials.username)}`)));
+          let b2 = await new Promise((resolve, reject) => (r = https.request(`https://inloggen.somtoday.nl/login?2-1.-passwordForm`, { method: "POST", headers: { 'Origin': 'https://inloggen.somtoday.nl', 'Content-Type': 'application/x-www-form-urlencoded', 'Cookie': cookie + "; " + cookie2 } }, (r) => r.on('data', (b) => { }).on('end', () => resolve(r.headers.location.split('/callback?code=')[1].split("&")[0]))), r.end(`loginLink=x&passwordFieldPanel%3ApasswordFieldPanel_body%3ApasswordField=${encodeURIComponent(credentials.password)}`)));
+          c = await new Promise((resolve, reject) => (d = '', r = https.request(`https://inloggen.somtoday.nl/oauth2/token`, { method: "POST", headers: { 'Content-Type': 'application/x-www-form-urlencoded', } }, (r) => r.on('data', (b) => d += b).on('end', () => resolve(d))), r.end(`code=${b2}&grant_type=authorization_code&client_id=D50E0C06-32D1-4B41-A137-A9A850C892C2`)));
+        } else {
+          let b = await new Promise((resolve, reject) => (r = https.request(`https://inloggen.somtoday.nl/?-1.-panel-signInForm&auth=${a}`, { method: "POST", headers: { 'Origin': 'https://inloggen.somtoday.nl', 'Content-Type': 'application/x-www-form-urlencoded', } }, (r) => r.on('data', (b) => { }).on('end', () => resolve(r.headers.location.split('/callback?code=')[1].split("&")[0]))), r.end(`loginLink=x&usernameFieldPanel%3AusernameFieldPanel_body%3AusernameField=${encodeURIComponent(credentials.username)}&passwordFieldPanel%3ApasswordFieldPanel_body%3ApasswordField=${encodeURIComponent(credentials.password)}`)));
+          c = await new Promise((resolve, reject) => (d = '', r = https.request(`https://inloggen.somtoday.nl/oauth2/token`, { method: "POST", headers: { 'Content-Type': 'application/x-www-form-urlencoded', } }, (r) => r.on('data', (b) => d += b).on('end', () => resolve(d))), r.end(`code=${b}&grant_type=authorization_code&client_id=D50E0C06-32D1-4B41-A137-A9A850C892C2`)));
+        }
+        this._storeInfo(JSON.parse(c));
+        this._authenticatedResolver(this);
+        this.axiosOptions = {
+          method: "get",
+          baseURL: `${this.somtodayApiUrl}` + endpoints.baseURL,
+          headers: { Authorization: `Bearer ${this.accessToken}` },
+        };
+      })();
     }
     this.refreshRefreshToken = this.refreshRefreshToken.bind(this);
   }
@@ -123,7 +120,7 @@ class User extends baseApiClass {
         },
       })
       .then((authResponse) => {
-        const { data }: { data: api_auth_item } = authResponse;
+        const { data }: { data: api_auth_item; } = authResponse;
         this._storeInfo(data);
         return true;
       })
@@ -132,7 +129,7 @@ class User extends baseApiClass {
           e.response.data.error === "invalid_grant" ||
           (e.response.data.error === "access_denied" &&
             e.response.data.error_description ===
-              "Access denied by resource owner or authorization server: Unauthorized account")
+            "Access denied by resource owner or authorization server: Unauthorized account")
         ) {
           log("Unable to refresh token");
           console.error(e);
